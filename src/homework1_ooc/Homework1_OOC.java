3eaeafb63099dcb63b4fd77ffbe09ac405854539
C2022188 <2022188@student.cct.ie>
2023-09-28 19:29:39 +0100

    Initial commit

---
+++ src/homework1_ooc/Homework1_OOC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package homework1_ooc;
+
+/**
+ *
+ * @author caroo
+ */
+public class Homework1_OOC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
